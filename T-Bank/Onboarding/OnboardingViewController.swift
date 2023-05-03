e778acd10a39d52a22230bbc8c2955cfff523bc6
Alexander <a.pride.xxi@tutanota.com>
2023-05-03 17:27:25 +0300

    Create generic onboaring view controller

--- T-Bank/Onboarding/OnboardingViewController.swift
+++ T-Bank/Onboarding/OnboardingViewController.swift
@@ -13,34 +13,50 @@ class OnboardingViewController: UIViewController {
     let imageView = UIImageView()
     let label = UILabel()
     
+    let stringImageName: String
+    let titleText: String
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .black
+        //view.backgroundColor = .black
         style()
         layout()
+    }
+    
+    init(stringImageName: String, titleText: String) {
+        self.stringImageName = stringImageName
+        self.titleText = titleText
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
 
 extension OnboardingViewController {
     
     private func style() {
+        view.backgroundColor = .systemBackground
+        
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
         stackView.spacing = 5
         
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
-        imageView.tintColor = .gray
-        imageView.image = UIImage(named: "onboarding_1")
+        imageView.tintColor = .black
+        imageView.image = UIImage(systemName: stringImageName)
         
         label.translatesAutoresizingMaskIntoConstraints = false
         label.textAlignment = .center
         label.font = UIFont.preferredFont(forTextStyle: .title3)
         label.adjustsFontForContentSizeCategory = true
         label.numberOfLines = 0
-        label.textColor = .white
-        label.text = "T-bank is faster, easier to use, and has brand new look and feel that will make you feel like you are back in 1989."
+        label.textColor = .black
+        label.text = titleText
     }
     
     private func layout() {
@@ -49,10 +65,10 @@ extension OnboardingViewController {
         view.addSubview(stackView)
         
         NSLayoutConstraint.activate([
-            
-            imageView.widthAnchor.constraint(equalToConstant: 200),
-            imageView.heightAnchor.constraint(equalToConstant: 150),
-            
+
+//            imageView.widthAnchor.constraint(equalToConstant: 150),
+//            imageView.heightAnchor.constraint(equalToConstant: 150),
+
             stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
